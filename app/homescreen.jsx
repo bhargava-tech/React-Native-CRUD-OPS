ec7f0aa0a6c858aa8d9f19de8a9c0e130a81a16c
bhargava.tech <tech.bhargavareddy@gmail.com>
2025-06-15 23:38:42 +0530

    Secure Storage mode updated

--- app/homescreen.jsx
+++ app/homescreen.jsx
@@ -5,7 +5,6 @@ import {
   Platform,
   StyleSheet,
   ScrollView,
-  FlatList,
   TextInput,
   Pressable,
   SafeAreaView,
@@ -13,21 +12,51 @@ import {
 import { todoList } from "@/data/Todos";
 import deleteIcon from "@/assets/images/delete.png";
 import addIcon from "@/assets/images/add-icon.png";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { ThemeContext } from "@/context/ThemeContext";
 import { Molengo_400Regular, useFonts } from "@expo-google-fonts/molengo";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import Animated, {
   LinearTransition as AnimType,
 } from "react-native-reanimated";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function HomePage() {
   const Container = Platform.OS === "web" ? ScrollView : SafeAreaView;
   const { colorScheme, setColorScheme, theme } = useContext(ThemeContext);
 
-  const [todos, setTodos] = useState(todoList.sort((a, b) => b.id - a.id));
+  const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState(``);
   const [loaded, error] = useFonts({ Molengo_400Regular });
+
+  useEffect(() => {
+    const fetchAsync = async () => {
+      try {
+        const jsonData = await AsyncStorage.getItem("TODODATA");
+        const remoteData = jsonData != null ? JSON.parse(jsonData) : null;
+        if (remoteData && remoteData.length) {
+          setTodos(remoteData.sort((a, b) => b.id - a.id));
+        } else {
+          setTodos(todoList.sort((a, b) => b.id - a.id));
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchAsync();
+  }, [todoList]);
+
+  useEffect(() => {
+    const updateAsync = async () => {
+      try {
+        const updateData = JSON.stringify(todos);
+        await AsyncStorage.setItem("TODODATA", updateData);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    updateAsync();
+  }, [todos]);
 
   if (!loaded && !error) {
     return null;
@@ -61,21 +90,24 @@ export default function HomePage() {
   return (
     <Container style={styles.container}>
       <View style={styles.mainView}>
-        <TextInput
-          style={styles.input}
-          placeholder="Add new todo"
-          value={newTodo}
-          onChangeText={setNewTodo}
-          placeholderTextColor="gray"
-        />
+        <View style={styles.subView}>
+          <TextInput
+            style={styles.input}
+            placeholder="Add new todo"
+            value={newTodo}
+            onChangeText={setNewTodo}
+            placeholderTextColor="gray"
+          />
+          <Pressable
+            onPress={() => {
+              onAddPress();
+            }}
+          >
+            <Image style={styles.addimage} source={addIcon} />
+          </Pressable>
+        </View>
         <Pressable
-          onPress={() => {
-            onAddPress();
-          }}
-        >
-          <Image style={styles.addimage} source={addIcon} />
-        </Pressable>
-        <Pressable
+          style={styles.dynamicTheme}
           onPress={() => {
             setColorScheme(colorScheme === "dark" ? "light" : "dark");
           }}
@@ -128,13 +160,55 @@ function createStyles(theme, colorScheme) {
       paddingRight: 10,
       paddingBottom: 30,
     },
+    mainView: {
+      flexDirection: "row",
+    },
+    subView: {
+      borderColor: "gray",
+      borderWidth: 1,
+      flex: 1,
+      flexDirection: "row",
+      borderRadius: 5,
+      padding: 5,
+    },
+    dynamicTheme: {
+      borderColor: "gray",
+      marginLeft: "10",
+      borderWidth: 1,
+      borderRadius: 5,
+      padding: 10,
+    },
+    input: {
+      height: 40,
+      paddingHorizontal: 10,
+      fontSize: 16,
+      fontFamily: "Molengo_400Regular",
+      borderRightWidth: 1,
+      marginEnd: 10,
+      borderRightColor: "gray",
+      flex: 1,
+      color: theme.text,
+    },
+    addimage: {
+      width: 30,
+      marginEnd: 5,
+      marginLeft: 5,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 5,
+      height: 30,
+    },
     flatList: {
       marginTop: 10,
       borderWidth: 1,
       borderRadius: 5,
       borderColor: "gray",
     },
-
+    image: {
+      width: 24,
+      height: 24,
+      alignItems: "flex-end",
+    },
     view: {
       flex: 1,
       padding: 20,
@@ -145,52 +219,21 @@ function createStyles(theme, colorScheme) {
       borderBottomEndRadius: 5,
       borderBottomColor: "gray",
     },
+
     textNormal: {
       color: theme.text,
-      fontSize: 16,
+      fontSize: 15,
       fontFamily: "Molengo_400Regular",
       flex: 1,
       fontWeight: "bold",
     },
     textRemoved: {
       color: "gray",
-      fontSize: 16,
+      fontSize: 15,
       fontFamily: "Molengo_400Regular",
       flex: 1,
       textDecorationLine: "line-through",
       fontWeight: "bold",
-    },
-    image: {
-      width: 24,
-      height: 24,
-      alignItems: "flex-end",
-    },
-    input: {
-      height: 40,
-      paddingHorizontal: 10,
-      fontSize: 15,
-      fontFamily: "Molengo_400Regular",
-      borderRightWidth: 1,
-      marginEnd: 10,
-      borderRightColor: "gray",
-      flex: 1,
-      color: theme.text,
-    },
-    mainView: {
-      flexDirection: "row",
-      borderColor: "gray",
-      borderWidth: 1,
-      borderRadius: 5,
-      padding: 5,
-    },
-    addimage: {
-      width: 30,
-      marginEnd: 5,
-      marginLeft: 5,
-      alignItems: "center",
-      justifyContent: "center",
-      marginTop: 2,
-      height: 30,
     },
   });
 }
